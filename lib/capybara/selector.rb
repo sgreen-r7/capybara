8a3108a8cde568b66a1e1cba8c2dc2c122d54928
Thomas Walpole <twalpole@gmail.com>
2016-10-05 15:16:00 -0700

    Allow specification of expression filters outside the main selector expression

--- lib/capybara/selector.rb
+++ lib/capybara/selector.rb
@@ -65,18 +65,19 @@ end
 # @filter [Boolean] :disabled Match disabled field?
 # @filter [Boolean] :multiple Match fields that accept multiple values
 Capybara.add_selector(:field) do
-  xpath(:name, :placeholder, :type) do |locator, options|
+  xpath(:name, :placeholder) do |locator, options|
     xpath = XPath.descendant(:input, :textarea, :select)[~XPath.attr(:type).one_of('submit', 'image', 'hidden')]
-    if options[:type]
-      type=options[:type].to_s
-      if ['textarea', 'select'].include?(type)
-        xpath = XPath.descendant(type.to_sym)
-      else
-        xpath = xpath[XPath.attr(:type).equals(type)]
-      end
-    end
     xpath=locate_field(xpath, locator, options)
     xpath
+  end
+
+  expression_filter(:type) do |expr, type|
+    type = type.to_s
+    if ['textarea', 'select'].include?(type)
+      expr.axis(:self, type.to_sym)
+    else
+      expr[XPath.attr(:type).equals(type)]
+    end
   end
 
   filter_set(:_field) # checked/unchecked/disabled/multiple
@@ -87,7 +88,7 @@ Capybara.add_selector(:field) do
   end
   describe do |options|
     desc = String.new
-    (expression_filters - [:type]).each { |ef| desc << " with #{ef} #{options[ef]}" if options.has_key?(ef) }
+    (expression_filters.keys - [:type]).each { |ef| desc << " with #{ef} #{options[ef]}" if options.has_key?(ef) }
     desc << " of type #{options[:type].inspect}" if options[:type]
     desc << " with value #{options[:with].to_s.inspect}" if options.has_key?(:with)
     desc
@@ -198,7 +199,7 @@ Capybara.add_selector(:button) do
 
     res_xpath = input_btn_xpath + btn_xpath + image_btn_xpath
 
-    res_xpath = expression_filters.inject(res_xpath) { |memo, ef| memo[find_by_attr(ef, options[ef])] }
+    res_xpath = expression_filters.keys.inject(res_xpath) { |memo, ef| memo[find_by_attr(ef, options[ef])] }
 
     res_xpath
   end
@@ -509,7 +510,7 @@ Capybara.add_selector(:frame) do
   xpath(:name) do |locator, options|
     xpath = XPath.descendant(:iframe) + XPath.descendant(:frame)
     xpath = xpath[XPath.attr(:id).equals(locator.to_s) | XPath.attr(:name).equals(locator)] unless locator.nil?
-    xpath = expression_filters.inject(xpath) { |memo, ef| memo[find_by_attr(ef, options[ef])] }
+    xpath = expression_filters.keys.inject(xpath) { |memo, ef| memo[find_by_attr(ef, options[ef])] }
     xpath
   end
 
